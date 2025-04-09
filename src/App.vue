d3efb6b991dd274b6354bc8a4760819c7107ef35
Lina Wion <lina@test.com>
2025-04-09 17:14:32 +0200

    first component created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-  <div id="app">
-   
-  </div>
+
+
+    <BaseCard>
+      <h1>Willkommen!</h1>
+      <p>Das ist eine wiederverwendbare Karte mit einem flexiblen Inhalt.</p>
+    </BaseCard>
+
+
 </template>
 
 <script>
+
+import BaseCard from './components/BaseCard.vue';
 
 
 export default {
   name: 'App',
   components: {
-
+    BaseCard
   }
 }
 </script>
@@ -18,11 +25,14 @@ export default {
 <style>
 body {
   margin: 0;
+ 
 }
 
 #app {
-  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
-  height: 100vh;
   width: 100vw;
+  min-height: 100vh;
+  padding: 100px;
+  box-sizing: border-box;
+  background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
 }
 </style>
---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="base-card">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "BaseCard",
+  };
+  </script>
+  
+  <style scoped>
+  .base-card {
+    background-color: #011f35;
+    border-radius: 16px;
+    padding: 20px 32px;
+    color: #ffffff; /* Standardmäßige Textfarbe für besseren Kontrast */
+  }
+  </style>
